2eaf6c8d275071dcdfeea4742b7d268009ca38fe
kongdeis <kongdeis@outlook.com>
2026-07-28 14:39:39 +0900

    init mono

---
+++ be-nest/src/app.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello() {
+    const dbUrl = process.env.DATABASE_URL ?? '(미설정)';
+    const conStr = process.env.BLOB_CONNECTION_STRING ?? '(미설정)';
+
+    return {
+      message: 'Hello World!-app service',
+      DATABASE_URL: dbUrl,
+      BLOB_CONNECTION_STRING: conStr,
+    }
+  }
+}
